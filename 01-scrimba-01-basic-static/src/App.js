14a5aa16ca88c74d7cd0f559a15b79b3d81d6907
karalkal <karalkal@yahoo.co.uk>
2022-11-26 20:27:09 +0000

    very first static project

---
+++ 01-scrimba-01-basic-static/src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Footer from "./Footer"
+import MainContent from "./MainContent";
+import Header from "./Header";
+
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
