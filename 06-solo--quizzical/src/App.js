ff09fee5c2079fe05d2512c565cfcbb25b59d3f5
karalkal <karalkal@yahoo.co.uk>
2023-02-08 01:52:38 +0000

    started quiz app

---
+++ 06-solo--quizzical/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+      <div className="flex-container">
+        <h1 >Quizzical</h1>
+        <h2> Test your knowledge</h2>
+        <button >Start quiz</button>
+      </div>
+  );
+}
+
+export default App;
