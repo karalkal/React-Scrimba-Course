ff024c633c7a57ba1c8e7fe2b5e75d101e9eaf1e
karalkal <karalkal@yahoo.co.uk>
2023-02-20 07:07:17 +0000

    added BG and color palette, styled welcome buttons

--- 06-solo--quizzical/src/Welcome.js
+++ 06-solo--quizzical/src/Welcome.js
@@ -8,10 +8,10 @@ export default function Welcome(props) {
         ["Books", "fa-solid fa-book", 10],
         ["Film", "fa-solid fa-film", 11],
         ["Music", "fa-solid fa-music", 12],
-        ["Science/Nature", "fa-solid fa-tree", 17],
-        ["Science/Computers", "fa-solid fa-laptop", 18],
-        ["Science/Maths", "fa-solid fa-plus-minus", 19],
-        ["Science/Gadgets", "fa-solid fa-headphones", 30],
+        ["Nature", "fa-solid fa-tree", 17],
+        ["Computers", "fa-solid fa-laptop", 18],
+        ["Maths", "fa-solid fa-plus-minus", 19],
+        ["Gadgets", "fa-solid fa-headphones", 30],
         ["Mythology", "fa-solid fa-mars-and-venus", 20],
         ["Sports", "fa-solid fa-futbol", 21],
         ["Geography", "fa-solid fa-globe", 22],
@@ -20,7 +20,6 @@ export default function Welcome(props) {
         ["Art", "fa-solid fa-palette", 25],
         ["Animals", "fa-solid fa-hippo", 27],
         ["Vehicles", "fa-solid fa-car-side", 28],
-
     ]
 
     return (
@@ -31,10 +30,10 @@ export default function Welcome(props) {
             <div id="categories--container">
                 {categories.map(cat =>
                     <button
-                        class="category--buttons"
+                        className="category--buttons"
                         key={cat[1]}
                         onClick={() => props.handleClick(cat[2])}>
-                        <i class={cat[1]}></i>
+                        <i className={cat[1]}></i>
                         {cat[0]}
                     </button>
                 )}
