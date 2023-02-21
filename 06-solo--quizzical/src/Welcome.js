d93f0f5758f14e87bd7de6a2c527787d82a51da0
karalkal <karalkal@yahoo.co.uk>
2023-02-21 07:52:25 +0000

    gather user input for selecting question category, type, difficulty before onSubmit

--- 06-solo--quizzical/src/Welcome.js
+++ 06-solo--quizzical/src/Welcome.js
@@ -3,6 +3,8 @@ import React from "react"
 
 export default function Welcome(props) {
 
+    const [categorySelected, setCategorySelected] = React.useState([])
+
     const categories = [
         ["General", "fa-solid fa-shuffle", 9],
         ["Books", "fa-solid fa-book", 10],
@@ -22,6 +24,10 @@ export default function Welcome(props) {
         ["Vehicles", "fa-solid fa-car-side", 28],
     ]
 
+    function selectCategory(cat) {
+        console.log(cat)
+    }
+
     return (
         <div className="welcome--container--flex">
             <h1 id="h1--welcome" >Quick Quiz</h1>
@@ -33,24 +39,35 @@ export default function Welcome(props) {
                     return (<button
                         className="category--buttons"
                         key={cat[1]}
-                        onClick={() => props.handleClick(cat)}>
+                        onClick={() => setCategorySelected(cat)}>
                         <i className={catIcon}></i>
                         {catName}
                     </button>)
                 }
                 )}
 
-                <div className="slidecontainer">
-                    <p>Custom range slider:</p>
-                    <input type="range" min="1" max="100" value="50" className="slider" id="myRange" />
+
+                <div id="quiz--difficulty">
+                    <p>select Difficulty Level:</p>
+                    <div id="difficulty--spans">
+                        <button id="dificulty--easy">Easy</button>
+                        <button id="dificulty--medium">Medium</button>
+                        <button id="dificulty--hard">Hard</button>
+                    </div>
                 </div>
 
+                <div id="quiz--type">
+                <p>select Questions Type:</p>
+                    <div id="quiz--type--spans">
+                        <button id="type--true--false">True / False</button>
+                        <button id="type--multichoice">Multi-Choice</button>
+                    </div>
+                </div>
             </div>
 
-
-
-
-
+            <button onClick={() => props.handleClick(categorySelected)}>
+                GO!
+            </button>
         </div>
     )
 }
